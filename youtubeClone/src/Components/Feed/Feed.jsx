f47ce4103168ab10ae92f5ee939b0146a5cdd99b
mmudgal33 <mmudgal33@gmail.com>
2025-09-20 23:45:36 +0530

    first commit

---
+++ youtubeClone/src/Components/Feed/Feed.jsx
@@ -0,0 +1,141 @@
+import React, { useEffect, useState } from 'react'
+import './Feed.css'
+import {Link} from 'react-router-dom'
+
+import {API_KEY} from '../../data'
+
+import thumbnail1 from '../../assets/thumbnail1.png'
+import thumbnail2 from '../../assets/thumbnail2.png'
+import thumbnail3 from '../../assets/thumbnail3.png'
+import thumbnail4 from '../../assets/thumbnail4.png'
+import thumbnail5 from '../../assets/thumbnail5.png'
+import thumbnail6 from '../../assets/thumbnail6.png'
+import thumbnail7 from '../../assets/thumbnail7.png'
+import thumbnail8 from '../../assets/thumbnail8.png'
+
+
+const Feed = ({category}) => {
+
+    const [data,setData] = useState([]);
+
+    const fetchData = async () => {
+        const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`;
+        await fetch(videoList_url).then(response=>response.json()).then(data=>setData(data.items))
+        console.log(data)
+    }
+
+    useEffect(()=>{
+        fetchData();
+    },[category])
+
+    // when category change, useEffect run
+
+
+    return (
+        <div className="feed">
+        {data.map((item,index)=>{
+            return(
+                <Link to={`video/${item.snippet.categoryId}/${item.id}`} className='card'>
+                <img src={item.snippet.thumbnails.medium.url} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </Link>
+            )
+        })}
+            
+            {/* <div className='card'>
+                <img src={thumbnail2} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail3} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail4} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail5} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail6} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail7} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail8} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail1} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail2} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail3} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail4} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail5} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail6} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail7} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail8} alt="" />
+                <h2>Best web developing tool for deep learning</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div> */}
+        </div>
+    )
+}
+
+export default Feed
